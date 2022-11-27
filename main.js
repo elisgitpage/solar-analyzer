248d7f49503f7b368c902ff3c8d182f63c74fef8
Eli Abidor <Eli.Abidor@infineon.com>
2022-11-27 16:35:17 -0500

    Made RGB font reactive and moved setup code to new method

--- main.js
+++ main.js
@@ -3,56 +3,23 @@
 
 // select img div and insert world GHI map from as CanvasRenderingContext2D
 // TODO: img div is hidden so really need to just pull data from image file directly
-const img = document.querySelector("#world-ghi");
-const worldImg = new Image(100, 200);
-worldImg.src = "./assets/World_GHI_mid-size-map_160x95mm-300dpi_v20191015.png";
-worldImg.id = "new-img";
+let rgbaImageData;
+let categoryAverages;
+let energyCategories;
+const colorLegendDims = { x: 43, y: 26 };
+const aimg = document.querySelector("#world-ghi");
+const img = new Image();
+img.src = "./assets/World_GHI_mid-size-map_160x95mm-300dpi_v20191015.png";
+img.id = "new-img";
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 
 let mainDiv = document.querySelector("#main");
 canvas.width = img.width;
 canvas.height = img.height;
-
-ctx.drawImage(img, 0, 0);
-
-console.log("Script is working!");
-
-// Pull Pixel data from Canvas
-const rgbaImageData = ctx.getImageData(0, 0, img.width, img.height);
-
-// Add canvas with image to main div as child
-//mainDiv.appendChild(canvas);
-mainDiv.insertBefore(canvas, mainDiv.children[0]);
-//mainDiv.appendChild(worldImg);
-const colorLegendDims = { x: 43, y: 26 };
-
-let energyCategories = buildEnergyCategories();
-
-console.log("Energy Categories:", energyCategories);
-
-let categoryAverages = calculateEnergyColorAverages();
-
-for (i = 0; i < 28; i++) {
-  logColor(categoryAverages[i], `Color category ${i}: `);
-}
-
-let globalSolarAverageDaily = calculateGlobalSolarEnergyAverage();
-
-let globalAverageDiv = document.getElementById("global-average");
-globalAverageDiv.innerHTML = `Global Daily Average Solar Energy: ${globalSolarAverageDaily.toFixed(
-  3
-)} kWh/m^2/day`;
-//canvas.after(globalAverageDiv);
-
-let pixelDataDiv = document.getElementById("pixel-data");
-//canvas.after(pixelDataDiv);
-
-canvas.addEventListener("mousedown", (e) => showPixelData(e));
-
-console.log("imageData length", rgbaImageData.data.length);
-
-let imageAvgColor = calculateWholeImageAverageColor();
+img.onload = () => {
+  initiateAnalysis();
+};
 
 // Draw rectangle with background color matching average pixel color of above image
 // let pixelColor = document.querySelector("#pixel-color");
@@ -70,6 +37,46 @@ let imageAvgColor = calculateWholeImageAverageColor();
 // .then(data => console.log(data));
 
 /*************************Function definition section********/
+
+function initiateAnalysis() {
+  ctx.drawImage(img, 0, 0);
+
+  console.log("Script is working!");
+
+  // Pull Pixel data from Canvas
+  rgbaImageData = ctx.getImageData(0, 0, img.width, img.height);
+
+  // Add canvas with image to main div as child
+  //mainDiv.appendChild(canvas);
+  mainDiv.insertBefore(canvas, mainDiv.children[0]);
+  //mainDiv.appendChild(worldImg);
+
+  energyCategories = buildEnergyCategories();
+
+  console.log("Energy Categories:", energyCategories);
+
+  categoryAverages = calculateEnergyColorAverages();
+
+  for (i = 0; i < 28; i++) {
+    logColor(categoryAverages[i], `Color category ${i}: `);
+  }
+
+  let globalSolarAverageDaily = calculateGlobalSolarEnergyAverage();
+
+  let globalAverageDiv = document.getElementById("global-average");
+  globalAverageDiv.innerHTML = `Global Daily Average Solar Energy: ${globalSolarAverageDaily.toFixed(
+    3
+  )} kWh/m^2/day`;
+  //canvas.after(globalAverageDiv);
+
+  //canvas.after(pixelDataDiv);
+
+  canvas.addEventListener("mousedown", (e) => showPixelData(e));
+
+  console.log("imageData length", rgbaImageData.data.length);
+
+  let imageAvgColor = calculateWholeImageAverageColor();
+}
 
 function fillLegendColors() {
   let x, y;
@@ -198,6 +205,7 @@ function showPixelData(event) {
   let rect = canvas.getBoundingClientRect();
   let x = event.clientX - rect.left;
   let y = event.clientY - rect.top;
+  let pixelDataDiv = document.getElementById("pixel-data");
   xGui = x.toFixed(3);
   yGui = y.toFixed(3);
   pixelDataDiv.children[0].textContent = `X-coordinate: ${xGui}, Y-coordinate: ${yGui}`;
@@ -214,10 +222,12 @@ function showPixelData(event) {
   );
   let inGreySpect = inGreySpectrum(pixelColor);
   console.log("Pixel in grey spectrum?: ", inGreySpect);
+  let greySpectdiv = document.getElementById("grey-spectrum");
   if (!inGreySpect) {
-    let greySpectdiv = document.getElementById("grey-spectrum");
     greySpectdiv.innerHTML = "In Grey Spectrum? NO";
     let closestCategory = findClosestCategory(pixelColor, categoryAverages);
+    colorSpan.style.color =
+      closestCategory <= 12 && closestCategory >= 7 ? "black" : "white";
     let avgEnergy = energyCategories[closestCategory];
     let colorLogMessage = `closest category: ${i}, daily energy: ${avgEnergy.daily} kWh/m^2, yearly energy: ${avgEnergy.yearly} GWh/km^2/yr, color: `;
     let energyCategoryDiv = document.getElementById("energy-category");
